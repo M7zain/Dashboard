2cbb7bb476a9b3ff1a99f5749c65b6aea81352ed
M7zain <zeynkara91@gmail.com>
2024-07-23 14:36:25 +0300

    added sql queries to createInvoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 'use server'; 
 import { z } from 'zod'; 
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -18,4 +20,15 @@ export async function createInvoice(formData: FormData){
         amount     : formData.get('amount'), 
         status     : formData.get('status'), 
 
-    });}
+    });
+    
+    const amountInCents = amount * 100; 
+    const date = new Date().toISOString().split('T')[0]; 
+
+    await sql `
+            INSER INTO invoices (customer_id, amount, status , date)
+            VALUES (${customerId}, ${amountInCents} , ${status} ,${date})
+            `;
+
+    revalidatePath('/dashboard/invoices'); 
+}
