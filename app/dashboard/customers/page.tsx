17a764afa4fb75f09af40e5a21f7fc83781fc7dd
M7zain <zeynkara91@gmail.com>
2024-07-26 10:23:52 +0300

    adding customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import CustomersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
- 
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
 export const metadata: Metadata = {
   title: 'customers',
 };
 
-const page = () => {
+export default async function(){
+  const query = ''; 
+  const customers = await fetchFilteredCustomers(query);
   return (
-    <div>customers</div>
-  )
+    <div>
+      <CustomersTable customers={customers}/>
+    </div>
+  ); 
 }
 
-export default page
