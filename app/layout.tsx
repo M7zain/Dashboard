bb280d82e37323f33a7a791491b9f74e7cb0c6d2
M7zain <zeynkara91@gmail.com>
2024-07-26 10:06:31 +0300

    dynamic metadata to invoices page

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,10 @@ import { Metadata } from 'next';
 
 
 export const metadata: Metadata = {
-  title: 'Acme Dashboard',
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
   description: 'The official Next.js Course Dashboard, built with App Router.',
   metadataBase: new URL('https://the-new-dashboard.vercel.app'),
 };
